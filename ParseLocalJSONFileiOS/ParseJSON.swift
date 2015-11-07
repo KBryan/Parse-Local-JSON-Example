2bec014dc3b8e710aef2433df4e04e5ed6d41291
KBryan <kwame@3einteractive.com>
2015-11-07 14:56:57 -0500

    Added some Markdown commenting.
    Markdown documenting found in the book on the subject Swift
    Documentation Markup

--- ParseLocalJSONFileiOS/ParseJSON.swift
+++ ParseLocalJSONFileiOS/ParseJSON.swift
@@ -15,13 +15,18 @@ struct ParseJSON {
     private var typeOfFile:String!
     var data:NSData!
     var stringData:NSString!
-    
+
     init(fName:String, fType:String) {
         nameOfFile = fName
         typeOfFile = fType
         parseJSONFile(nameOfFile, fileType: typeOfFile)
     }
-
+    /// Parses a local JSON file and returns a data object
+    /// - Parameters
+    /// - param1 the name of the local file
+    /// - param2 the extension type of the file in this case .json
+    /// [JSON Documentation](
+    ///  https://developer.apple.com/library/ios/documentation/Foundation/Reference/NSJSONSerialization_Class/)
     mutating func parseJSONFile(fileName:String, fileType:String) -> NSData {
         
         // Do any additional setup after loading the view, typically from a nib.
